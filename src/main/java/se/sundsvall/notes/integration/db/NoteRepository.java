add113a325026a1dd13cb53f19160b00c213d0a7
Joakim Schürer <joakim.schurer@sundsvall.se>
2023-03-02 14:49:47 +0100

    UF-4484: Adjusted circuit breaker settings
    - Added transactional annotation to repository interface
    - Updated Dept44 to version 1.30

--- src/main/java/se/sundsvall/notes/integration/db/NoteRepository.java
+++ src/main/java/se/sundsvall/notes/integration/db/NoteRepository.java
@@ -1,19 +1,23 @@
 package se.sundsvall.notes.integration.db;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import se.sundsvall.notes.api.model.FindNotesRequest;
-import se.sundsvall.notes.integration.db.model.NoteEntity;
-
 import static se.sundsvall.notes.integration.db.specification.NoteSpecification.withCaseId;
 import static se.sundsvall.notes.integration.db.specification.NoteSpecification.withClientId;
 import static se.sundsvall.notes.integration.db.specification.NoteSpecification.withContext;
 import static se.sundsvall.notes.integration.db.specification.NoteSpecification.withPartyId;
 import static se.sundsvall.notes.integration.db.specification.NoteSpecification.withRole;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import se.sundsvall.notes.api.model.FindNotesRequest;
+import se.sundsvall.notes.integration.db.model.NoteEntity;
+
+@Transactional
 @CircuitBreaker(name = "NoteRepository")
 public interface NoteRepository extends JpaRepository<NoteEntity, String>, JpaSpecificationExecutor<NoteEntity> {
 
