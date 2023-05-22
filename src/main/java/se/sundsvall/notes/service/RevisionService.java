39277ff6be835d25e57674899c5790965655ea06
Michael Wallner <Michael.Wallner@Sundsvall.se>
2023-05-22 15:51:31 +0200

    UF-5234: Create log-events for CUD-operations.

--- src/main/java/se/sundsvall/notes/service/RevisionService.java
+++ src/main/java/se/sundsvall/notes/service/RevisionService.java
@@ -1,5 +1,27 @@
 package se.sundsvall.notes.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flipkart.zjsonpatch.DiffFlags;
+import com.flipkart.zjsonpatch.JsonDiff;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.zalando.problem.Problem;
+import se.sundsvall.notes.api.model.DifferenceResponse;
+import se.sundsvall.notes.api.model.Operation;
+import se.sundsvall.notes.api.model.Revision;
+import se.sundsvall.notes.integration.db.RevisionRepository;
+import se.sundsvall.notes.integration.db.model.NoteEntity;
+import se.sundsvall.notes.integration.db.model.RevisionEntity;
+import se.sundsvall.notes.service.mapper.RevisionMapper;
+
+import java.io.IOException;
+import java.util.EnumSet;
+import java.util.List;
+
 import static com.flipkart.zjsonpatch.DiffFlags.ADD_ORIGINAL_VALUE_ON_REPLACE;
 import static com.flipkart.zjsonpatch.DiffFlags.OMIT_VALUE_ON_REMOVE;
 import static java.lang.String.format;
@@ -8,31 +30,10 @@ import static org.zalando.problem.Status.INTERNAL_SERVER_ERROR;
 import static org.zalando.problem.Status.NOT_FOUND;
 import static se.sundsvall.notes.service.ServiceConstants.PROBLEM_DURING_DIFF;
 import static se.sundsvall.notes.service.ServiceConstants.REVISION_NOT_FOUND_FOR_ID_AND_VERSION;
+import static se.sundsvall.notes.service.mapper.RevisionMapper.toRevision;
+import static se.sundsvall.notes.service.mapper.RevisionMapper.toRevisionEntity;
 import static se.sundsvall.notes.service.mapper.RevisionMapper.toRevisionList;
-
-import java.io.IOException;
-import java.util.EnumSet;
-import java.util.List;
-
-import jakarta.transaction.Transactional;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.zalando.problem.Problem;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.flipkart.zjsonpatch.DiffFlags;
-import com.flipkart.zjsonpatch.JsonDiff;
-
-import se.sundsvall.notes.api.model.DifferenceResponse;
-import se.sundsvall.notes.api.model.Operation;
-import se.sundsvall.notes.api.model.Revision;
-import se.sundsvall.notes.integration.db.RevisionRepository;
-import se.sundsvall.notes.integration.db.model.NoteEntity;
-import se.sundsvall.notes.integration.db.model.RevisionEntity;
+import static se.sundsvall.notes.service.mapper.RevisionMapper.toSerializedSnapshot;
 
 @Service
 @Transactional
@@ -90,6 +91,35 @@ public class RevisionService {
 	 * - no previous revisions exist for the provided entity.
 	 *
 	 * @param entity the entity that will have a new revision.
+	 * @return the created revision.
+	 */
+	public Revision createNoteRevision(NoteEntity entity) {
+
+		final var lastRevision = revisionRepository.findFirstByEntityIdOrderByVersionDesc(entity.getId());
+
+		if (lastRevision.isPresent()) {
+
+			// No changes since last revision, return.
+			if (jsonEquals(lastRevision.get().getSerializedSnapshot(), toSerializedSnapshot(entity))) {
+				return null;
+			}
+
+			// Create revision <lastRevision.version + 1>
+			return toRevision(createAndReturnRevision(entity, lastRevision.get().getVersion() + 1));
+		}
+
+		// No previous revisions exist. Create revision 0
+		return toRevision(createAndReturnRevision(entity, 0));
+	}
+
+	/**
+	 * Create a new revision.
+	 *
+	 * A new revision will be created if:
+	 * - the last revisions serialized-snapshot differs from the current (i.e. provided) entity.
+	 * - no previous revisions exist for the provided entity.
+	 *
+	 * @param entity the entity that will have a new revision.
 	 * @return the id (uuid) of the created revision.
 	 */
 	public String createRevision(final NoteEntity entity) {
@@ -115,6 +145,18 @@ public class RevisionService {
 		return toRevisionList(revisionRepository.findAllByEntityIdOrderByVersion(noteEntityId));
 	}
 
+	/**
+	 * Returns the lastest (current) revision of the note
+	 *
+	 * @param noteId id of the note to fetch latest revision for.
+	 * @return the latest revision for the note or null if note does not exist.
+	 */
+	public Revision getLatestNoteRevision(String noteId) {
+		return revisionRepository.findFirstByEntityIdOrderByVersionDesc(noteId)
+			.map(RevisionMapper::toRevision)
+			.orElse(null);
+	}
+
 	private String toJsonString(final NoteEntity entity) {
 		try {
 			return objectMapper.writeValueAsString(entity);
@@ -131,6 +173,10 @@ public class RevisionService {
 			.withEntityType(entity.getClass().getSimpleName())
 			.withSerializedSnapshot(toJsonString(entity))
 			.withVersion(version)).getId();
+	}
+
+	private RevisionEntity createAndReturnRevision(final NoteEntity entity, final int version) {
+		return revisionRepository.save(toRevisionEntity(entity, version));
 	}
 
 	private boolean jsonEquals(final String json1, final String json2) {
---
+++ src/test/java/se/sundsvall/notes/service/mapper/EventMapperTest.java
@@ -0,0 +1,109 @@
+package se.sundsvall.notes.service.mapper;
+
+import generated.se.sundsvall.eventlog.EventType;
+import generated.se.sundsvall.eventlog.Metadata;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import se.sundsvall.notes.api.model.Note;
+import se.sundsvall.notes.api.model.Revision;
+import se.sundsvall.notes.integration.db.model.NoteEntity;
+
+import java.time.OffsetDateTime;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
+import static java.util.Collections.emptyMap;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
+import static org.assertj.core.groups.Tuple.tuple;
+
+class EventMapperTest {
+
+	@Test
+	void toEvent() {
+		final var eventType = EventType.CREATE;
+		final var message = "message";
+		final var revision = "revision";
+		final var metaData = Map.of("key", "value");
+		final var executedByUserId = "executedByUserId";
+		final var owner = "Notes";
+		final var sourceType = Note.class.getSimpleName();
+
+		// Setup
+
+		// Call
+		final var event = EventMapper.toEvent(eventType, message, revision, metaData, executedByUserId);
+
+		// Verification
+		assertThat(event).isNotNull();
+		assertThat(event.getCreated()).isNotNull().isCloseTo(OffsetDateTime.now(), within(1, SECONDS))   ;
+		assertThat(event.getType()).isEqualTo(eventType);
+		assertThat(event.getMessage()).isEqualTo(message);
+		assertThat(event.getHistoryReference()).isEqualTo(revision);
+		assertThat(event.getOwner()).isEqualTo(owner);
+		assertThat(event.getSourceType()).isEqualTo(sourceType);
+		assertThat(event.getMetadata()).hasSize(2).extracting(
+				Metadata::getKey,
+				Metadata::getValue)
+			.containsExactly(tuple("key", "value"),
+				tuple("ExecutedBy", executedByUserId));
+	}
+
+	@Test
+	void toEventWithNullValues() {
+
+		// Call
+		final var event = EventMapper.toEvent(null, null, null, null, null);
+
+		// Verification
+		assertThat(event).isNull();
+	}
+
+	@Test
+	void toEventWithEmptyValues() {
+
+		// Call
+		final var event = EventMapper.toEvent(EventType.CREATE, "", "", Map.of(), "");
+
+		// Verification
+		assertThat(event).isNotNull();
+		assertThat(event.getCreated()).isNotNull().isCloseTo(OffsetDateTime.now(), within(1, SECONDS))   ;
+		assertThat(event.getType()).isEqualTo(EventType.CREATE);
+		assertThat(event.getMessage()).isEmpty();
+		assertThat(event.getHistoryReference()).isEmpty();
+		assertThat(event.getOwner()).isEqualTo("Notes");
+		assertThat(event.getSourceType()).isEqualTo(Note.class.getSimpleName());
+		assertThat(event.getMetadata()).hasSize(1).extracting(
+				Metadata::getKey,
+				Metadata::getValue)
+			.containsExactly(tuple("ExecutedBy", ""));
+	}
+
+	@ParameterizedTest
+	@MethodSource("metadataMapArgumentProvider")
+	void toMetadataMap(NoteEntity noteEntity, Revision currentRevision, Revision previousRevision, Map<String, String> result) {
+		assertThat(EventMapper.toMetadataMap(noteEntity, currentRevision, previousRevision)).containsExactlyInAnyOrderEntriesOf(result);
+	}
+
+	private static Stream<Arguments> metadataMapArgumentProvider() {
+		final var CASE_ID = "caseId";
+		final var PREVIOUS_ID = "previousId";
+		final var PREVIOUS_VERSION = 1;
+		final var KEY_CURRENT_REVISION = "CurrentRevision";
+		final var KEY_CURRENT_VERSION = "CurrentVersion";
+		final var KEY_PREVIOUS_VERSION = "PreviousVersion";
+		final var KEY_PREVIOUS_REVISION = "PreviousRevision";
+		final var CURRENT_ID = "currentId";
+		final var CURRENT_VERSION = 2;
+		final var KEY_CASE_ID = "CaseId";
+		return Stream.of(
+			Arguments.of(NoteEntity.create().withCaseId(CASE_ID), null, null, Map.of(KEY_CASE_ID, CASE_ID)),
+			Arguments.of(null, null, Revision.create().withId(PREVIOUS_ID).withVersion(PREVIOUS_VERSION), Map.of(KEY_PREVIOUS_REVISION, PREVIOUS_ID, KEY_PREVIOUS_VERSION, String.valueOf(PREVIOUS_VERSION))),
+			Arguments.of(null, Revision.create().withId(CURRENT_ID).withVersion(CURRENT_VERSION), null, Map.of(KEY_CURRENT_REVISION, CURRENT_ID, KEY_CURRENT_VERSION, String.valueOf(CURRENT_VERSION))),
+			Arguments.of(null, null, null, emptyMap()));
+	}
+
+}
