7904106d16865b8d7efc24bfb1282f1a77f126bc
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2023-07-31 08:49:35 +0200

    UF-5566: Switch to Sonar.

--- src/test/java/se/sundsvall/notes/service/mapper/RevisionMapperTest.java
+++ src/test/java/se/sundsvall/notes/service/mapper/RevisionMapperTest.java
@@ -1,22 +1,21 @@
 package se.sundsvall.notes.service.mapper;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import se.sundsvall.notes.api.model.Revision;
-import se.sundsvall.notes.integration.db.model.NoteEntity;
-import se.sundsvall.notes.integration.db.model.RevisionEntity;
+import static java.time.OffsetDateTime.now;
+import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
-import static java.time.OffsetDateTime.now;
-import static java.util.Collections.emptyList;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.tuple;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import se.sundsvall.notes.api.model.Revision;
+import se.sundsvall.notes.integration.db.model.NoteEntity;
+import se.sundsvall.notes.integration.db.model.RevisionEntity;
 
 @ExtendWith(MockitoExtension.class)
 class RevisionMapperTest {
