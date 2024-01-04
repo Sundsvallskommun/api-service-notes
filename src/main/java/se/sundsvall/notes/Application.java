1e1b8b764cb152a98bc3c2a86f5fe8f0a2b5f288
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2024-01-04 15:10:15 +0100

    UF-6619: Add CI

--- src/main/java/se/sundsvall/notes/Application.java
+++ src/main/java/se/sundsvall/notes/Application.java
@@ -1,10 +1,12 @@
 package se.sundsvall.notes;
 
-import se.sundsvall.dept44.ServiceApplication;
-
 import static org.springframework.boot.SpringApplication.run;
 
+import se.sundsvall.dept44.ServiceApplication;
+import se.sundsvall.dept44.util.jacoco.ExcludeFromJacocoGeneratedCoverageReport;
+
 @ServiceApplication
+@ExcludeFromJacocoGeneratedCoverageReport
 public class Application {
 	public static void main(String... args) {
 		run(Application.class, args);
